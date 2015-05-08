044fa695b36b54fc40fecad70906399c97ea0be6
Viktor Fonic <viktor.fonic@gmail.com>
2015-05-08 18:27:45 +0100

    rails_apps_composer: add pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  mount Upmin::Engine => '/admin'
+  root to: 'visitors#index'
   devise_for :users
+  resources :users
   mount RailsAdmin::Engine => '/rails_admin', as: 'rails_admin'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
