69723a28dea246ba97cc3581267133575ba8eb18
Viktor Fonic <viktor.fonic@gmail.com>
2015-05-12 22:44:26 +0100

    Add pagination to API

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::UsersController < Api::V1::ApiController
+  after_filter only: [:show] { set_pagination_header(:microposts) }
+
   def show
     @user = User.friendly.find(params[:username])
-    @microposts = @user.microposts
+    @microposts = @user.microposts.page params[:page]
     render json: @microposts
   end
 end
