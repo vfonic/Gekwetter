cef8d608de74cd3e6b244eb700ac592b09f68a10
Viktor Fonic <viktor.fonic@gmail.com>
2015-05-12 19:06:03 +0100

    Force non-www on rails level

--- config/routes.rb
+++ config/routes.rb
@@ -94,4 +94,8 @@ Rails.application.routes.draw do
       get :following, :followers
     end
   end
+
+  constraints subdomain: 'www' do
+    get ':any', to: redirect(subdomain: nil, path: '/%{any}'), any: /.*/
+  end
 end
