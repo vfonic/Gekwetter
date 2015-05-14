d471d58f221668788eb23910b40340461d324f5d
Viktor Fonic <viktor.fonic@gmail.com>
2015-05-14 21:27:11 +0100

    Implement new status posting using bootbox modal window

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :new_status_box_if_signed_in
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+    def new_status_box_if_signed_in
+      if user_signed_in?
+        @new_micropost = Micropost.new
+      end
+    end
+
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
       devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
