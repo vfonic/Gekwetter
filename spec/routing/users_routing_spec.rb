0f04774e0421e21f55e0281bea1b0505b1f891cf
Viktor Fonic <viktor.fonic@gmail.com>
2015-05-17 21:22:49 +0100

    DRY route specs

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -2,20 +2,9 @@ require "rails_helper"
 
 describe UsersController, type: :routing do
   describe "users routing" do
-    it "routes to #index" do
-      expect(get: "/users").to route_to("users#index")
-    end
-
-    it "routes to #show" do
-      expect(get: "/vfonic").to route_to("users#show", id: "vfonic")
-    end
-
-    it "routes to #following" do
-      expect(get: "/vfonic/following").to route_to("users#following", id: "vfonic")
-    end
-
-    it "routes to #followers" do
-      expect(get: "/vfonic/followers").to route_to("users#followers", id: "vfonic")
-    end
+    it { should route(:get, '/users').to(action: :index) }
+    it { should route(:get, '/vfonic').to(action: :show, id: 'vfonic') }
+    it { should route(:get, '/vfonic/following').to(action: :following, id: 'vfonic') }
+    it { should route(:get, '/vfonic/followers').to(action: :followers, id: 'vfonic') }
   end
 end
