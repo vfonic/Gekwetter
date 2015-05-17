d786a608433459d0d3a736e74de9283665d3aeac
Viktor Fonic <viktor.fonic@gmail.com>
2015-05-17 21:22:49 +0100

    DRY route specs

--- spec/routing/microposts_routing_spec.rb
+++ spec/routing/microposts_routing_spec.rb
@@ -2,12 +2,7 @@ require "rails_helper"
 
 describe MicropostsController, type: :controller do
   describe "microposts routing" do
-    it "routes to #create" do
-      expect(post: '/vfonic/microposts').to route_to('microposts#create', user_id: 'vfonic')
-    end
-
-    it "routes to #destroy" do
-      expect(delete: '/vfonic/microposts/1').to route_to('microposts#destroy', user_id: 'vfonic', id: '1')
-    end
+    it { should route(:post, '/vfonic/microposts').to(action: :create, user_id: 'vfonic') }
+    it { should route(:delete, '/vfonic/microposts/1').to(action: :destroy, user_id: 'vfonic', id: '1') }
   end
 end
